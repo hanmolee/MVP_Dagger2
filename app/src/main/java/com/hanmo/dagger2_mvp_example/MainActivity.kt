bee2603324ec0f8b11162785209a88ab60049207
hanmolee <dlgksah@gmail.com>
2018-12-11 22:27:54 +0900

    create project

---
+++ app/src/main/java/com/hanmo/dagger2_mvp_example/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hanmo.dagger2_mvp_example
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
